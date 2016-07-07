83e7fc3f9f556d7537406da45f037e22684bb7d4
Elise Huard <elise@mastodonc.com>
2016-07-07 13:15:22 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,18 @@
+(def slf4j-version "1.7.21")
+(defproject whiner-slf4j "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [org.clojure/tools.logging      "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [org.slf4j/jul-to-slf4j         ~slf4j-version]
+                 [org.slf4j/jcl-over-slf4j       ~slf4j-version]
+                 [org.slf4j/log4j-over-slf4j     ~slf4j-version]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler whiner.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
---
+++ src/whiner/handler.clj
@@ -0,0 +1,28 @@
+(ns whiner.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.tools.logging :as log]))
+
+(defroutes app-routes
+  (GET "/info" []
+       (log/info "informative message")
+       "Info")
+  (GET "/warn" []
+       (log/warn "warning")
+       "Warning")
+  (GET "/error" []
+       (log/error "error")
+       "Error")
+  (GET "/exception" []
+       (do
+         (throw (Exception. "this is an exception"))
+         "never reached"))
+  (GET "/runtime" []
+       (do
+         (quot 1 0)
+         "never reached"))
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
